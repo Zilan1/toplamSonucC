5d103078261ce24158c7c186621dc6d4f6748924
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:04:52 +0300

    sum

---
+++ proje16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	float sayi1,sayi2,toplam;
+	
+	printf("Birinci Sayiyi Giriniz: ");
+	scanf("%f",&sayi1);
+    
+	printf("Ikinci Sayiyi Giriniz: ");
+	scanf("%f",&sayi2);
+	
+	toplam=sayi1+sayi2
+
+	printf("Toplama Sonucu: ");
+	scanf("%f",&toplam);
+		
+	return 0;
+}
